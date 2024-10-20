ff67a634b4b4648a9aa6d786548b193fa4936f13
Tobilahs <carmen.albarran.1932@gmail.com>
2024-10-20 13:18:04 -0600

    iniciado

---
+++ FINIXNEW/language.js
@@ -0,0 +1,7 @@
+global.spesialprem = 'Only premium on database'
+global.spesialown = 'Only owner on database'
+global.notext = '!send text!'
+global.noingroup = 'Only group feature'
+global.nobotadmin = 'Only bot admin feature'
+global.usernoadmin = 'Only admin feature'
+global.gabisafix = "Sorry, your number is not in the database"
---
+++ console.js
@@ -0,0 +1,11 @@
+// OBEY COMMANDMENTS
+
+global.prefa = ['','!','.',',','🐤','🗿'] //NOT CHANGE
+global.owner = ['6285727819741'] //OWNER
+global.botname = 'FatalCrash Finix' //BOT NAME
+global.versisc = "4.5.0" //NOT CHANGE
+global.baileys1 = require('@whiskeysockets/baileys') //NOT CHANGE
+global.creator = "Tama_Ultraa" //NOT CHANGE
+global.packname = "Sticker By" //OPSIONAL
+global.author = "Tama_Ultraa" //OPSIONAL
+global.simbol = "!" // SIMBOL MENU
